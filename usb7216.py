fb3feb5209bf04b7d36a49b114760c97f7034143
Olivier Huguenot <olivier.huguenot@nimbleone.io>
2023-03-03 11:22:14 +0100

    fix: cleanup

--- usb7216.py
+++ usb7216.py
@@ -99,7 +99,7 @@ def write_config_register(bus: SMBus, offset: int, bytes: bytearray):
 
 
 # -----------------------------------------------------------------------------
-def usb_attach(bus: SMBus, during_runtime: bool = False):
+def usb_attach(bus: SMBus, during_runtime: bool = True):
     w = i2c_msg.write(I2C_SLAVE_ADDR,
                       [0xaa, 0x55 + (1 if during_runtime else 0), 0x00])
     bus.i2c_rdwr(w)
@@ -118,7 +118,7 @@ def debug_bytearray(msg: str, bytes: bytearray):
 # -----------------------------------------------------------------------------
 with SMBus(BUS_ADDR) as bus:
 
-    # usb_attach(bus, True)
+    usb_attach(bus)
 
     # should be: bytearray(b'\x05\xa2\x00\xc1')
     device_revision_register = read_config_register(bus, 0x0000, 4)
@@ -137,105 +137,19 @@ with SMBus(BUS_ADDR) as bus:
     debug_bytearray("vendor_id_reg: ", vendor_id_reg)
 
     # flexconnect port1 usb2+3
-    word = 0
-    word |= ((1 << 12) # port 1
-        | (1 << 11) # usb3 enable
-        | (0 << 8) # no timeout
-        | (0 << 7) # usb3 attach and reattach
-        | (0 << 6) # usb2 attach and reattach
-        | (1 << 5) # enable flexconnect
-        | (1 << 4) # usb2 enable
-        | 1) # port 1
+    # word = 0
+    # word |= ((1 << 12) # port 1
+    #     | (1 << 11) # usb3 enable
+    #     | (0 << 8) # no timeout
+    #     | (0 << 7) # usb3 attach and reattach
+    #     | (0 << 6) # usb2 attach and reattach
+    #     | (1 << 5) # enable flexconnect
+    #     | (1 << 4) # usb2 enable
+    #     | 1) # port 1
     # write_config_register(bus, 0x3440, [word >> 8, word & 0xff])
 
-    # flex usb2
+    # flex usb2 on port 1
     write_config_register(bus, 0x0808, [0x01])
 
-    # flex usb2
+    # flex usb3 on port 1
     write_config_register(bus, 0x0828, [0x01])
-
-    usb_attach(bus, True)
-
-    # ---
-    # table 277
-    # w = i2c_msg.write(I2C_SLAVE_ADDR, 
-    #                   [0x00, 0x00, # memory addr (???)
-    #                    0x08, # number of bytes to write to memory
-    #                    0x00, # write configuration register 
-    #                    0x02, # write two bytes to VID register
-    #                    0xbf, 0x80, 0x30, 0x00, # VID register addr is 0xbf803000
-    #                    0xad, 0xde]) # bytes to write
-    # bus.i2c_rdwr(w)
-    
-    # # ---
-    # # execute the 'Configuration Register Access' command
-    # w = i2c_msg.write(I2C_SLAVE_ADDR,
-    #                   [0x99, 0x37, 0x00]) # command 9937h + 00h (command completion)
-    # bus.i2c_rdwr(w)
-
-    # # check
-    # vendor_id_reg = read_config_register(bus, 0x3000, 2)
-    # debug_bytearray("vendor_id_reg: ", vendor_id_reg)
-
-
-
-    
-    # write the command block to the buffer area
-    # w = i2c_msg.write(I2C_SLAVE_ADDR, 
-    #                   [0x00, 0x00, # memory addr (???)
-    #                    0x06, # number of bytes to write to memory
-    #                    0x01, # read configuration register 
-    #                    0x04, # reading two bytes from PID register
-    #                    0xbf, 0x80, 0x00, 0x00]) # PID register addr is 0xbf803002
-    # bus.i2c_rdwr(w)
-
-    # # execute the 'Configuration Register Access' command
-    # w = i2c_msg.write(I2C_SLAVE_ADDR,
-    #                   [0x99, 0x37, 0x00]) # command 9937h + 00h (command completion)
-    # bus.i2c_rdwr(w)
-
-    # # read the data from memory, starting at memory offset 06h, which is where the data byte starts
-    # # w = i2c_msg.write(I2C_SLAVE_ADDR, 
-    # #                   [0x00, 0x06, # for read access at offset 0006h
-    # #                    (I2C_SLAVE_ADDR << 1) + 0x01, # client addr and read bit
-    # #                    0x08, # device sends a count of 8 bytes
-    # #                    0x16, # PID LSB
-    # #                    0x49]) # PID MSB
-    # # bus.i2c_rdwr(w)
-    # w = i2c_msg.write(I2C_SLAVE_ADDR, 
-    #                   [0x00, 0x06]) # for read access at offset 0006h      
-    # bus.i2c_rdwr(w)
-
-    # r = i2c_msg.read(I2C_SLAVE_ADDR, 0x08)# client addr and read bit
-    # bus.i2c_rdwr(r)
-    
-    # for dat in r:
-    #     print(hex(dat))
-
-    # # device revision register: 32bits
-    # def read32(offset: int) -> int:
-    #     u32 = 0
-    #     for idx in range(0, 4):
-    #         u8 = bus.read_byte_data(I2C_SLAVE_ADDR, offset + idx)
-    #         print(f"read u8: {hex(u8)} at addr {hex(offset + idx)}")
-    #         u32 |= u8 << (idx * 8)
-    #     print(f"read u32: {hex(u32)} from addr {hex(offset)}")
-    #     return u32
-
-    # device_revision = read32(0x0)
-
-    # print(f"device ID: {hex((device_revision & 0xffff) >> 16)}")
-    # print(f"device revID: {hex(device_revision & 0xff)}")
-
-    # bus.close()
-
-# with SMBus(1) as bus:
-#     # Device Revision Register
-#     for i in range(0, 4):
-#         b = bus.read_byte_data(SMBUS_CONFIG_ADDR, 0xbf80_0000 + i)
-#         print("byte: " + hex(b))
-#     print("all read")
-#         # except (TimeoutError, OSError) as err:
-#         #     print(".", end="")
-#     # dword = bus.read_i2c_block_data(SMBUS_CONFIG_ADDR, 0xbf80, 4)
-#     # print(dword)
